05c9518c51bb82c55eab33615aa709296be0da19
YacineHallo <108801103+YacineHallo@users.noreply.github.com>
2023-02-14 16:40:08 +0100

    Iets meer opmaak Charactercomponent, nog lang niet klaar > aparate Scss gemaakt

--- src/views/homeview/HomeView.tsx
+++ src/views/homeview/HomeView.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { PageWrapper } from "../../components/pagewrapper/PageWrapper";
 import { CharacterComponent } from "../../components/charactercomponent/CharacterCOmponent";
 import { ICharacter, ICharactersResponse } from "../../types/IndexTypes";
+import { IdentityCard } from "@amsterdam/asc-assets";
 
 export const HomeView = () => {
   const [character, setCharacter] = useState<ICharacter[]>([]);
@@ -31,7 +32,7 @@ export const HomeView = () => {
     <PageWrapper isLoading={loading}>
       <h1>Home</h1>
       {character.map((perCharacter: JSX.IntrinsicAttributes & ICharacter) => (
-        <CharacterComponent {...perCharacter} />
+        <CharacterComponent {...perCharacter} key={perCharacter.id} />
       ))}
     </PageWrapper>
   );
