0432662e8a5b75a37f58143cba8e8743ba5ec856
YacineHallo <108801103+YacineHallo@users.noreply.github.com>
2023-02-15 12:52:14 +0100

    Eerste opzet Paginering

--- src/views/homeview/HomeView.tsx
+++ src/views/homeview/HomeView.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { PageWrapper } from "../../components/pagewrapper/PageWrapper";
 import CharacterComponent from "../../components/charactercomponent/CharacterCOmponent";
+//import { Pagination } from "../../components/paginationComponent/PaginationComponent";
 import { ICharacter, ICharactersResponse } from "../../types/IndexTypes";
 
 export const HomeView = () => {
   const [character, setCharacter] = useState<ICharacter[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  type prev = number | null;
+  type next = number | null;
 
-  const getCharacters = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-redeclare
+  let prev = () => {
+    if (pageNumber! < 1) setPageNumber(pageNumber - 1);
+  };
+  // eslint-disable-next-line @typescript-eslint/no-redeclare
+  let next = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const getCharacters = async (newPage?: number) => {
     setLoading(true);
     try {
-      const api = `https://rickandmortyapi.com/api/character`;
+      const api = `https://rickandmortyapi.com/api/character/?page=${newPage}`;
       const res = await await fetch(`${api}`);
       const resJson: ICharactersResponse = await res.json();
       console.log("res ", resJson);
@@ -24,8 +37,8 @@ export const HomeView = () => {
   };
 
   useEffect(() => {
-    getCharacters();
-  }, []);
+    getCharacters(pageNumber);
+  }, [pageNumber]);
 
   return (
     <PageWrapper isLoading={loading}>
@@ -33,6 +46,14 @@ export const HomeView = () => {
         {character.map((perCharacter: JSX.IntrinsicAttributes & ICharacter) => (
           <CharacterComponent {...perCharacter} key={perCharacter.id} />
         ))}
+
+        <button onClick={prev} className="button-pagination">
+          Prev
+        </button>
+
+        <button onClick={next} className="button-pagination">
+          Next
+        </button>
       </div>
     </PageWrapper>
   );
